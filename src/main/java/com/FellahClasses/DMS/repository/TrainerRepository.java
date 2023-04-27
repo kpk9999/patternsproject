ed06faf2f57a7e2021896e724b1e3e68ba2b3c34
kpk9999 <112146534+kpk9999@users.noreply.github.com>
2023-04-27 18:21:28 -0500

    first commit

---
+++ src/main/java/com/FellahClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+package com.FellahClasses.DMS.repository;
+
+/**
+*
+* @author Praveen 
+
+*/
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.FellahClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
